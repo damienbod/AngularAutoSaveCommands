eb9e1740a141ddd2e556ec013273ea09c25d576f
damienbod <damien_bod@hotmail.com>
2016-08-26 10:32:32 +0200

    Adding data models which will be used in the commands

---
+++ src/Angular2AutoSaveCommands/Models/AboutData.cs
@@ -0,0 +1,9 @@
+﻿namespace Angular2AutoSaveCommands.Models
+{
+    public class AboutData
+    {
+        public long Id { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ src/Angular2AutoSaveCommands/Models/HomeForm.cs
@@ -0,0 +1,9 @@
+﻿namespace Angular2AutoSaveCommands.Models
+{
+    public class HomeData
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
