22f1d715ccf2e3873a5dd39a6f564479b6f1d575
damienbod <damien_bod@hotmail.com>
2016-08-26 10:22:58 +0200

    adding some validation for the controller post

--- src/Angular2AutoSaveCommands/Controllers/CommandController.cs
+++ src/Angular2AutoSaveCommands/Controllers/CommandController.cs
@@ -16,9 +16,33 @@ namespace Angular2AutoSaveCommands.Controllers
         [HttpPost]
         public IActionResult Post([FromBody]CommandDto value)
         {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest("Model is invalid");
+            }
+
+            if(!validateCommandType(value))
+            {
+                return BadRequest($"CommandType: {value.CommandType} is invalid");
+            }
+
+            if (!validatePayloadType(value))
+            {
+                return BadRequest($"PayloadType: {value.CommandType} is invalid");
+            }
+
             _commandHandler.Execute(value);
             return Ok(value);
         }
 
+        private bool validateCommandType(CommandDto value)
+        {
+            return true;
+        }
+
+        private bool validatePayloadType(CommandDto value)
+        {
+            return true;
+        }
     }
 }
