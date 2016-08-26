c54c14b7408b1ae066797cf00a0650bdaed273ce
damienbod <damien_bod@hotmail.com>
2016-08-26 09:46:49 +0200

    Adding static consts to the server

---
+++ src/Angular2AutoSaveCommands/Models/CommandTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Angular2AutoSaveCommands.Models
+{
+    public static  class CommandTypes
+    {
+        public const string ADD = "ADD";
+        public const string UPDATE = "UPDATE";
+        public const string DELETE = "DELETE";
+        public const string UNDO = "UNDO";
+        public const string REDO = "REDO";
+    }
+}
