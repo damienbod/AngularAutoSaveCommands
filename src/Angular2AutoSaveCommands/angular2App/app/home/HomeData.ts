67df88036b27da8ca47759e80f89ece18c85b738
damienbod <damien_bod@hotmail.com>
2016-09-06 20:32:54 +0200

    Extending the home component to use the commands

---
+++ src/Angular2AutoSaveCommands/angular2App/app/home/HomeData.ts
@@ -0,0 +1,14 @@
+﻿export class HomeData {
+    constructor(id: number, name: string, deleted: boolean) {
+
+        this.Id = id;
+        this.Name = name;
+        this.Deleted = deleted;
+
+    }
+
+    Id: number;
+    Name: string;
+    Deleted: boolean;
+}
+  
--- src/Angular2AutoSaveCommands/angular2App/app/home/home.component.ts
+++ src/Angular2AutoSaveCommands/angular2App/app/home/home.component.ts
@@ -1,29 +1,87 @@
 import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
-import { TestDataService } from '../services/testDataService';
-
+import { HomeData } from './HomeData';
+import { CommandService } from '../services/commandService';
+import { CommandDto } from '../services/commandDto';
+import { HomeDataService } from '../services/homeDataService';
 
 @Component({
     selector: 'homecomponent',
-    template: require('./home.component.html'),
-    providers: [TestDataService]
+    template: require('./home.component.html')
 })
 
 export class HomeComponent implements OnInit {
 
     public message: string;
-    public values: any[];
+    public model: HomeData;
+    public submitted: boolean;
+    public active: boolean;
+    public HomeDataItems: HomeData[];
 
-    constructor(private _dataService: TestDataService) {
-        this.message = "Hello from HomeComponent constructor";
+    constructor(private _commandService: CommandService, private _homeDataService: HomeDataService) {
+        this.message = "Hello from Home";
     }
 
     ngOnInit() {
-        this._dataService
-            .GetAll()
-            .subscribe(data => this.values = data,
+        this.model = new HomeData(0, 'new home name', false);
+        this.submitted = false;
+        this.active = true;
+        this.GetHomeDataItems();
+    }
+
+    public GetHomeDataItems() {
+        console.log('HomeComponent starting...');
+        this._homeDataService.GetAll()
+            .subscribe((data) => {
+                this.HomeDataItems = data;
+            },
             error => console.log(error),
-            () => console.log('Get all complete'));
+            () => {
+                console.log('HomeDataService:GetAll completed');
+            }
+        );
+    }
+
+    public Edit(aboutItem: HomeData) {
+        this.model.Name = aboutItem.Name;
+        this.model.Id = aboutItem.Id;
+    }
+
+    // TODO remove the get All request and update the list using the return item
+    public Delete(aboutItem: HomeData) {
+        let myCommand = new CommandDto("DELETE", "HOME", aboutItem, "home");
+
+        console.log(myCommand);
+        this._commandService.Execute(myCommand)
+            .subscribe(
+            data => this.GetHomeDataItems(),
+            error => console.log(error),
+            () => console.log('Command executed')
+            );
+    }
+
+    // TODO remove the get All request and update the list using the return item
+    public onSubmit() {
+        this.submitted = true;
+        let myCommand = new CommandDto("ADD", "HOME", this.model, "home");
+
+        if (this.model.Id > 0) {
+            myCommand.CommandType = "UPDATE";
+        }
+
+        console.log(myCommand);
+        this._commandService.Execute(myCommand)
+            .subscribe(
+            data => this.GetHomeDataItems(),
+            error => console.log(error),
+            () => console.log('Command executed')
+            );
+    }
+
+    public newHomeData() {
+        this.model = new HomeData(0, 'new home item', false);
+        this.active = false;
+        setTimeout(() => this.active = true, 0);
     }
 }
