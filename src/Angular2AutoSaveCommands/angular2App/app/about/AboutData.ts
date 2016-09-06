9c207bb0ee97e6b57f8a64dfacc4f61ef3a4b1e4
damienbod <damien_bod@hotmail.com>
2016-09-06 20:32:29 +0200

    Extending the about component to use the commands

---
+++ src/Angular2AutoSaveCommands/angular2App/app/about/AboutData.ts
@@ -0,0 +1,14 @@
+﻿export class AboutData {
+    constructor(id: number, description: string, deleted: boolean) {
+
+        this.Id = id;
+        this.Description = description;
+        this.Deleted = deleted;
+
+    }
+
+    Id: number;
+    Description: string;
+    Deleted: boolean;
+}
+  
--- src/Angular2AutoSaveCommands/angular2App/app/about/about.component.ts
+++ src/Angular2AutoSaveCommands/angular2App/app/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AboutData } from './AboutData';
+import { CommandService } from '../services/commandService';
+import { CommandDto } from '../services/commandDto';
+import { AboutDataService } from '../services/aboutDataService';
 
 @Component({
     selector: 'about',
@@ -8,12 +12,74 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
     public message: string;
+    public model: AboutData;
+    public submitted: boolean;
+    public active: boolean;
+    public AboutDataItems: AboutData[];
 
-    constructor() {
+    constructor(private _commandService: CommandService, private _aboutDataService: AboutDataService) {
         this.message = "Hello from About";
     }
 
     ngOnInit() {
-       
+        this.model = new AboutData(0, 'yes', false);
+        this.submitted = false;
+        this.active = true;
+        this.GetAboutDataItems();
+    }
+
+    public GetAboutDataItems() {
+        console.log('AboutComponent starting...');
+        this._aboutDataService.GetAll()
+            .subscribe((data) => {
+                this.AboutDataItems = data;
+            },
+            error => console.log(error),
+            () => {
+                console.log('AboutDataService:GetAll completed');
+            }
+        );
+    } 
+
+    public Edit(aboutItem: AboutData) {
+        this.model.Description = aboutItem.Description;
+        this.model.Id = aboutItem.Id;
+    }
+
+    // TODO remove the get All request and update the list using the return item
+    public Delete(aboutItem: AboutData) {
+        let myCommand = new CommandDto("DELETE", "ABOUT", aboutItem, "about");
+
+        console.log(myCommand);
+        this._commandService.Execute(myCommand)
+            .subscribe(
+            data => this.GetAboutDataItems(),
+            error => console.log(error),
+            () => console.log('Command executed')
+            );
+    }
+
+    // TODO remove the get All request and update the list using the return item
+    public onSubmit() {
+        this.submitted = true;
+        let myCommand = new CommandDto("ADD", "ABOUT", this.model, "about");
+
+        if (this.model.Id > 0) {
+            myCommand.CommandType = "UPDATE";
+        } 
+        
+        console.log(myCommand);
+        this._commandService.Execute(myCommand)
+            .subscribe(
+            data => this.GetAboutDataItems(),
+                error => console.log(error),
+                () => console.log('Command executed')
+            );
+    }
+
+    public newAboutData() {
+        this.model = new AboutData(0, 'yes', false);
+        this.active = false;
+        setTimeout(() => this.active = true, 0);
     }
 }
