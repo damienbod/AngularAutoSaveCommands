0ae4909010f4d79c8d6aec77a97a24ecf21d8fbf
damienbod <damien_bod@hotmail.com>
2016-09-06 20:30:18 +0200

    Adding services in SPA to consume the API from ASP.NET Core service

---
+++ src/Angular2AutoSaveCommands/angular2App/app/services/commandService.ts
@@ -0,0 +1,41 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'
+import { Observable } from 'rxjs/Observable';
+import { Configuration } from '../app.constants';
+import { CommandDto } from './CommandDto';
+
+@Injectable()
+export class CommandService {
+
+    private actionUrl: string;
+    private headers: Headers;
+
+    constructor(private _http: Http, private _configuration: Configuration) {
+
+        this.actionUrl = `${_configuration.Server}api/command/`;
+
+        this.headers = new Headers();
+        this.headers.append('Content-Type', 'application/json');
+        this.headers.append('Accept', 'application/json');
+    }
+
+    public Execute = (command: CommandDto): Observable<Response> => {
+        let url = `${this.actionUrl}execute`;
+        return this._http.post(url, command, { headers: this.headers }).map(res => res.json());
+    }
+
+    public Undo = (): Observable<Response> => {
+        let url = `${this.actionUrl}undo`;
+        return this._http.post(url, '', { headers: this.headers }).map(res => res.json());
+    }
+
+    public Redo = (): Observable<Response> => {
+        let url = `${this.actionUrl}redo`;
+        return this._http.post(url, '', { headers: this.headers }).map(res => res.json());
+    }
+
+    public GetAll = (): Observable<any> => {
+        return this._http.get(this.actionUrl).map((response: Response) => <any>response.json());
+    }
+}
