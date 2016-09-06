ab436262069c332bdf2287902df4026675d68e59
damienbod <damien_bod@hotmail.com>
2016-09-06 20:27:27 +0200

    Adding the select for the About and the Home lists

---
+++ src/Angular2AutoSaveCommands/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Angular2AutoSaveCommands.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly DomainModelMsSqlServerContext _context;
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly ILogger _logger;
+
+        public HomeController(DomainModelMsSqlServerContext context, ILoggerFactory loggerFactory)
+        {
+            _context = context;
+            _loggerFactory = loggerFactory;
+            _logger = loggerFactory.CreateLogger("HomeController");
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogDebug("Get all HomeData items called");
+            return Ok(_context.HomeData.Where(item => item.Deleted == false).ToList());
+        }
+    }
+}
