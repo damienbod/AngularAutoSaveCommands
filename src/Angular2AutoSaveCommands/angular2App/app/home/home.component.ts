355a95bc9039f62f0c2ef9ba4964958bff755f0a
damienbod <damien_bod@hotmail.com>
2016-09-08 04:29:20 +0200

    using deboucerTime with 1 second delay for input events

--- src/Angular2AutoSaveCommands/angular2App/app/home/home.component.ts
+++ src/Angular2AutoSaveCommands/angular2App/app/home/home.component.ts
@@ -1,10 +1,25 @@
 import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Http } from '@angular/http';
 import { HomeData } from './HomeData';
 import { CommandService } from '../services/commandService';
 import { CommandDto } from '../services/commandDto';
 import { HomeDataService } from '../services/homeDataService';
+
+import { Subject } from 'rxjs/Subject';
+
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
+
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
     selector: 'homecomponent',
@@ -18,12 +33,13 @@ export class HomeComponent implements OnInit {
     public submitted: boolean;
     public active: boolean;
     public HomeDataItems: HomeData[];
-
-    private executeTimer: any;
+    public deboucedInput: Observable<string>;
+    private keyDownEvents = new Subject<string>();
 
     constructor(private _commandService: CommandService, private _homeDataService: HomeDataService) {
         this.message = "Hello from Home";
         this._commandService.OnUndoRedo.subscribe(item => this.OnUndoRedoRecieved(item));
+
     }
 
     ngOnInit() {
@@ -31,6 +47,14 @@ export class HomeComponent implements OnInit {
         this.submitted = false;
         this.active = true;
         this.GetHomeDataItems();
+
+        this.deboucedInput = this.keyDownEvents;
+        this.deboucedInput
+            .debounceTime(1000)       
+            .distinctUntilChanged()   
+            .subscribe((filter: string) => {
+                this.onSubmit();
+            });
     }
 
     public GetHomeDataItems() {
@@ -65,35 +89,30 @@ export class HomeComponent implements OnInit {
     }
 
     public createCommand(evt: any) {
-        if (!this.executeTimer) {
-            this.executeTimer = setTimeout(
-                () => {
-                    this.onSubmit();
-                },
-                1000);
-        }
+        this.keyDownEvents.next(this.model.Name);
     }
 
     // TODO remove the get All request and update the list using the return item
     public onSubmit() {
-        this.executeTimer = undefined;
-        this.submitted = true;
-        let myCommand = new CommandDto("ADD", "HOME", this.model, "home");
+        if (this.model.Name != "") {
+            this.submitted = true;
+            let myCommand = new CommandDto("ADD", "HOME", this.model, "home");
 
-        if (this.model.Id > 0) {
-            myCommand.CommandType = "UPDATE";
-        }
+            if (this.model.Id > 0) {
+                myCommand.CommandType = "UPDATE";
+            }
 
-        console.log(myCommand);
-        this._commandService.Execute(myCommand)
-            .subscribe(
-            data => {
-                this.model.Id = data.Payload.Id;
-                this.GetHomeDataItems();
-            },
-            error => console.log(error),
-            () => console.log('Command executed')
-            );
+            console.log(myCommand);
+            this._commandService.Execute(myCommand)
+                .subscribe(
+                data => {
+                    this.model.Id = data.Payload.Id;
+                    this.GetHomeDataItems();
+                },
+                error => console.log(error),
+                () => console.log('Command executed')
+                );
+        }       
     }
 
     public newHomeData() {
