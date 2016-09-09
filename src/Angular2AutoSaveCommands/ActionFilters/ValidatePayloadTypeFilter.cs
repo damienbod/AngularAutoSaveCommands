51bc77b54d375c964aca2d92c80c9a42b832d127
damienbod <damien_bod@hotmail.com>
2016-09-09 09:39:17 +0200

    Add the action filters

---
+++ src/Angular2AutoSaveCommands/ActionFilters/ValidatePayloadTypeFilter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Angular2AutoSaveCommands.ActionFilters
+{
+    public class ValidatePayloadTypeFilter : ActionFilterAttribute
+    {
+        private readonly ILogger _logger;
+
+        public ValidatePayloadTypeFilter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger("ValidatePayloadTypeFilter");
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogWarning("ClassFilter OnActionExecuting");
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogWarning("ClassFilter OnActionExecuted");
+            base.OnActionExecuted(context);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            _logger.LogWarning("ClassFilter OnResultExecuting");
+            base.OnResultExecuting(context);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            _logger.LogWarning("ClassFilter OnResultExecuted");
+            base.OnResultExecuted(context);
+        }
+    }
+}
---
+++ src/Angular2AutoSaveCommands/ActionFilters/ValidationCommandTypeFilter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Angular2AutoSaveCommands.ActionFilters
+{
+    public class ValidationCommandTypeFilter : ActionFilterAttribute
+    {
+        private readonly ILogger _logger;
+
+        public ValidationCommandTypeFilter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger("ValidationCommandTypeFilter");
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogWarning("ClassFilter OnActionExecuting");
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogWarning("ClassFilter OnActionExecuted");
+            base.OnActionExecuted(context);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            _logger.LogWarning("ClassFilter OnResultExecuting");
+            base.OnResultExecuting(context);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            _logger.LogWarning("ClassFilter OnResultExecuted");
+            base.OnResultExecuted(context);
+        }
+    }
+}
