c7472f64e4e7bc470c626290e64ff23e82edbd22
damienbod <damien_bod@hotmail.com>
2016-09-09 09:45:29 +0200

    removing old validation templates in controller

--- src/Angular2AutoSaveCommands/Controllers/CommandController.cs
+++ src/Angular2AutoSaveCommands/Controllers/CommandController.cs
@@ -26,16 +26,6 @@ namespace Angular2AutoSaveCommands.Controllers
                 return BadRequest("Model is invalid");
             }
 
-            if (!validateCommandType(value))
-            {
-                return BadRequest($"CommandType: {value.CommandType} is invalid");
-            }
-
-            if (!validatePayloadType(value))
-            {
-                return BadRequest($"PayloadType: {value.CommandType} is invalid");
-            }
-
             _commandHandler.Execute(value);
             return Ok(value);
         }
@@ -55,19 +45,6 @@ namespace Angular2AutoSaveCommands.Controllers
             var commandDto = _commandHandler.Redo();
             return Ok(commandDto);
         }
-
-        // TODO
-        private bool validateCommandType(CommandDto value)
-        {
-            return true;
-        }
-
-        // TODO
-        private bool validatePayloadType(CommandDto value)
-        {
-            return true;
-        }
-
 
         [HttpGet]
         public IActionResult Get()
