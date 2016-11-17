1b6db17d36a8003b6e59cfa913eab9697979d49a
damienbod <damien_bod@hotmail.com>
2016-11-17 01:06:43 +0100

    adding new build for app

--- src/Angular2AutoSaveCommands/angular2App/polyfills.ts
+++ src/Angular2AutoSaveCommands/angular2App/polyfills.ts
@@ -1,7 +1,5 @@
-import 'ie-shim'; // Internet Explorer 9 support
+import 'ie-shim'; // Internet Explorer 9 support.
 
-// import 'core-js/es6';
-// Added parts of es6 which are necessary for your project or your browser support requirements.
 import 'core-js/es6/symbol';
 import 'core-js/es6/object';
 import 'core-js/es6/function';
@@ -19,29 +17,8 @@ import 'core-js/es6/weak-map';
 import 'core-js/es6/weak-set';
 import 'core-js/es6/typed';
 import 'core-js/es6/reflect';
-// see issue https://github.com/AngularClass/angular2-webpack-starter/issues/709
-// import 'core-js/es6/promise';
-
 import 'core-js/es7/reflect';
+
 import 'zone.js/dist/zone';
 
-
-// Polyfill(s) for older browsers.
-//import 'core-js/client/core';
-
-//import 'reflect-metadata';
-//require('zone.js/dist/zone');
-
 import 'ts-helpers';
-
-if (process.env.NODE_ENV === 'production') {
-  // Production
-
-} else {
-  // Development
-
-  Error['stackTraceLimit'] = Infinity;
-
-  require('zone.js/dist/long-stack-trace-zone');
-  //import 'zone.js/dist/long-stack-trace-zone';
-}
--- src/Angular2AutoSaveCommands/angular2App/vendor.ts
+++ src/Angular2AutoSaveCommands/angular2App/vendor.ts
@@ -1,30 +1,4 @@
-﻿import '@angular/platform-browser-dynamic';
-import '@angular/platform-browser';
-import '@angular/core';
-import '@angular/http';
-import '@angular/router';
-
-
-// RxJS 5
-// import 'rxjs/Rx';
-
-// RxJS.
-//import 'rxjs';
-
-//// Angular 2.
-//import '@angular/core';
-//import '@angular/common';
-//import '@angular/compiler';
-//import '@angular/forms';
-//import '@angular/http';
-//import '@angular/platform-browser';
-//import '@angular/platform-browser-dynamic';
-//import '@angular/router';
-//import '@angular/upgrade';
-
-
-// Other libraries.
-import 'jquery/src/jquery';
+﻿import 'jquery/src/jquery';
 import 'bootstrap/dist/js/bootstrap';
 
 import './css/bootstrap.css';
