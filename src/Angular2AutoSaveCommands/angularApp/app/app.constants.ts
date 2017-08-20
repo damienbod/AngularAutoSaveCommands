7d532f53453fa74a775673da7dc097a2f54f0312
damienbod <damien_bod@hotmail.com>
2017-08-20 13:19:55 +0200

    Updating ASP.NET Core 2.0

--- src/Angular2AutoSaveCommands/angularApp/app/app.constants.ts
+++ src/Angular2AutoSaveCommands/angularApp/app/app.constants.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public Server = 'http://localhost:5805/';
+    public Server = 'http://localhost:41001/';
 }
