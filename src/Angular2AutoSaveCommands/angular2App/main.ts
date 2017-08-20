14f95c73576dbbd6cff2e7ccf41162f62a19e21e
damienbod <damien_bod@hotmail.com>
2017-08-20 11:11:33 +0200

    updating Angular app

--- src/Angular2AutoSaveCommands/angular2App/main.ts
+++
@@ -1,8 +0,0 @@
-﻿// Entry point for JiT compilation.
-export * from './polyfills';
-export * from './vendor';
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
