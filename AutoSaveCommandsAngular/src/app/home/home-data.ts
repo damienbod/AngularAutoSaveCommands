9df756064045c023a2a59cc22dab7b742add7857
damienbod <damien_bod@hotmail.com>
2021-01-19 23:07:42 +0100

    fixing home component

--- AutoSaveCommandsAngular/src/app/home/home-data.ts
+++ AutoSaveCommandsAngular/src/app/home/home-data.ts
@@ -1,13 +1,13 @@
 ﻿export class HomeData {
 
-    public Id = 0;
-    public Name = '';
-    public Deleted = false;
+    public id = 0;
+    public name = '';
+    public deleted = false;
 
     constructor(id: number, name: string, deleted: boolean) {
 
-        this.Id = id;
-        this.Name = name;
-        this.Deleted = deleted;
+        this.id = id;
+        this.name = name;
+        this.deleted = deleted;
     }
 }
