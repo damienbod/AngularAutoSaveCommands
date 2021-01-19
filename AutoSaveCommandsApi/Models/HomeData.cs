8e9b61eafebd6a67ca190ac624afc7a10d4bbad9
damienbod <damien_bod@hotmail.com>
2021-01-19 22:14:17 +0100

    Init .NET 5 API project

---
+++ AutoSaveCommandsApi/Models/HomeData.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutoSaveCommandsApi.Models
+{
+    public class HomeData
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Deleted { get; set; }
+    }
+}
---
+++ AutoSaveCommandsApi/Providers/ICommandDataAccessProvider.cs
@@ -0,0 +1,12 @@
+﻿using AutoSaveCommandsApi.Models;
+
+namespace AutoSaveCommandsApi.Providers
+{
+    public interface ICommandDataAccessProvider
+    {
+        void AddCommand(CommandEntity command);
+
+        void Save();
+
+    }
+}
