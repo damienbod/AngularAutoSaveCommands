0eb964d0c3f828b6ad98f469bc5f142c80121cd9
damienbod <damien_bod@hotmail.com>
2021-01-19 23:30:03 +0100

    updating .NET 5

--- AutoSaveCommandsApi/Startup.cs
+++ AutoSaveCommandsApi/Startup.cs
@@ -2,18 +2,11 @@ using AutoSaveCommandsApi.ActionFilters;
 using AutoSaveCommandsApi.Providers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AutoSaveCommandsApi
 {
@@ -53,7 +46,7 @@ namespace AutoSaveCommandsApi
                     });
             });
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AutoSaveCommandsApi", Version = "v1" });
